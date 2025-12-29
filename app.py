90b02a458220aed26f6a6c3a61576bed6037974e
andryianna <andrea.iannaccone001@studenti.uniparthenope.it>
2025-12-29 18:18:12 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from db import db
+from routes.connect import connect_bp
+from routes.query import query_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+app.register_blueprint(connect_bp)
+app.register_blueprint(query_bp)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
