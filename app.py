5faf034dfc2840d039abde2723ff1615b60430b4
andryianna <andrea.iannaccone001@studenti.uniparthenope.it>
2026-01-04 19:35:00 +0100

    boh

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sqlalchemy import func
 import os
 
 from dbModels.Resort import Resort
@@ -25,20 +26,36 @@ db.init_app(app)
 init()
 @app.route("/", methods=["GET"])
 def index():
+    query = Resort.query
+
     name = request.args.get("name")
     region = request.args.get("region")
-
-    query = Resort.query
+    min_altitude = request.args.get("min_altitude")
+    min_slopes = request.args.get("min_slopes")
+    order = request.args.get("order")
 
     if name:
         query = query.filter(Resort.name.ilike(f"%{name}%"))
+
     if region:
         query = query.filter_by(region=region)
+
+    if min_altitude:
+        query = query.filter(Resort.altitude_max >= int(min_altitude))
+
+    if min_slopes:
+        query = query.outerjoin(Resort.slopes) \
+                     .group_by(Resort.id) \
+                     .having(func.count() >= int(min_slopes))
+
+    if order == "name":
+        query = query.order_by(Resort.name)
+    elif order == "altitude":
+        query = query.order_by(Resort.altitude_max.desc())
 
     resorts = query.all()
 
     return render_template("index.html", resorts=resorts)
-
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
